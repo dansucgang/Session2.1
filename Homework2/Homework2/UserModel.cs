185dd97fc9a2ed0cc2c35104b607293b896d5d88
Danife Sucgang <danife.sucgang@ph.softvision.global>
2022-10-10 19:26:54 +0800

    Session2.1Homework

---
+++ Homework2/Homework2/UserModel.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework2
+{
+    public class UserModel
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("category")]
+        public Category Category { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("photoUrls")]
+        public string[] PhotoUrls { get; set; }
+
+        [JsonProperty("tags")]
+        public List<Tags> Tags { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+    }
+}
