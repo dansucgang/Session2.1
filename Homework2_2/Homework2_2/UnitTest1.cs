f28d6def9e61eddf2d95afba99be687cc4f3e005
Danife Sucgang <danife.sucgang@ph.softvision.global>
2022-10-13 14:39:15 +0800

    Session2.2

---
+++ Homework2_2/Homework2_2/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Homework2_2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
